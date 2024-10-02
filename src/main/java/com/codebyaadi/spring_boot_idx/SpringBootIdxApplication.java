3976d51eea605e8dba16932c3499d69aacaf4ffd
adityastar57 <adityastar57@gmail.com>
2024-10-02 16:11:50 +0000

    initialized spring boot template for idx

---
+++ src/main/java/com/codebyaadi/spring_boot_idx/SpringBootIdxApplication.java
@@ -0,0 +1,13 @@
+package com.codebyaadi.spring_boot_idx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIdxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootIdxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codebyaadi/spring_boot_idx/SpringBootIdxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codebyaadi.spring_boot_idx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIdxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
